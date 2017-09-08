7d8c979c36b1b05549c5006d90b96075b7f289e9
neelesh38 <neeleshbansal38@gmail.com>
2017-09-08 22:01:14 +0530

    Evaluation of Brackets

--- evaluateexp.cpp
+++ evaluateexp.cpp
@@ -1,49 +1,59 @@
-#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+int top=-1;
 char stack[100];
-int top=1;
-void push(char val)
+void push(char s)
 {
     top=top+1;
-    stack[top]=val;
+    stack[top]=s;
 }
 char pop()
 {
     if(top==-1)
     {
-        cout<<"list is empty";
-        return 0;
+      return 0;
     }
     return stack[top--];
 }
-/*char peakvalue()
-{
-    return stack[top];
-}*/
-int main()
-{
-    int i,flag=0;
+int main() {
     char t;
-    string s="{[()]}";
-    for (i=0;s[i]!='\0';i++)
+    string st;
+     int n,i,j,flag=0;
+     cin>>n;
+    for(i=0;i<n;i++)
     {
-        t=s[i];
-        if(t=='('||t=='{'||t=='[')
+        cin>>st;
+        for(j=0;st[j]!='\0';j++)
+        {
+            cout<<"top"<<top;
+            t=st[j];
+            if((top==-1) && t==']'||t=='}'||t==')')
+            {
+              flag=1;
+            }
+            if(t=='('||t=='{'||t=='[')
             {
                 push(t);
             }
-        else if(t==')' && stack[top]=='(')
-            pop();
-        else if(t=='}' && stack[top]=='{')
-            pop();
-        else if(t==']' && stack[top]=='[')
-            pop();
+            else if(t==')' && stack[top]=='(')
+            {
+                cout<<pop();
+                flag=0;
+            }
+            else if(t==']' && stack[top]=='[')
+                cout<<pop();
+            else if(t=='}' && stack[top]=='{')
+                cout<<pop();
+
+        }
+        if(top==-1 && flag==0)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        //top=-1;
     }
-    if(pop()==0)
-        cout<<"bal";
-         else
-            cout<<"not bal";
-
-
-
 }
