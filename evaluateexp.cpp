d359476d30911ad8f756e1c41d09b83c15f81892
neelesh38 <neeleshbansal38@gmail.com>
2017-09-08 22:04:01 +0530

    Evaluation of Brackets

--- evaluateexp.cpp
+++ evaluateexp.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 int top=-1;
-char stack[100];
+char stack[1000];
 void push(char s)
 {
     top=top+1;
@@ -23,17 +23,16 @@ int main() {
     char t;
     string st;
      int n,i,j,flag=0;
-     cin>>n;
+    cin>>n;
     for(i=0;i<n;i++)
     {
         cin>>st;
         for(j=0;st[j]!='\0';j++)
         {
-            cout<<"top"<<top;
             t=st[j];
-            if((top==-1) && t==']'||t=='}'||t==')')
+            if((top==-1)&& t==')'||t==']'||t=='}')
             {
-              flag=1;
+                flag=1;
             }
             if(t=='('||t=='{'||t=='[')
             {
@@ -41,19 +40,27 @@ int main() {
             }
             else if(t==')' && stack[top]=='(')
             {
-                cout<<pop();
+                pop();
                 flag=0;
             }
+
             else if(t==']' && stack[top]=='[')
-                cout<<pop();
+            {
+                pop();
+                flag=0;
+            }
+
             else if(t=='}' && stack[top]=='{')
-                cout<<pop();
+            {
+                pop();
+                flag=0;
+            }
 
         }
         if(top==-1 && flag==0)
-            cout<<"YES";
+            cout<<"YES"<<endl;
         else
-            cout<<"NO";
-        //top=-1;
+            cout<<"NO"<<endl;
+        top=-1;
     }
 }
